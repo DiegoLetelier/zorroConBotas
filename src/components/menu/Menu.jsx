2935acedb3b651813374251c0cc3016e2171a03a
Diego Letelier <104950177+DiegoLetelier@users.noreply.github.com>
2022-10-08 21:38:35 -0300

    image on menu centered

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,8 +1,8 @@
-
+import './menu.css'
 
 const Menu = () => {
     return (
-      <div>
+      <div className="center">
           <h2> MENU</h2>
           <div>
           <img src='https://images.unsplash.com/photo-1568031813264-d394c5d474b9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80'
